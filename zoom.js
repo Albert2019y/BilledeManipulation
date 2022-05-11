757b04df264d9d0678626ed3d1460f94ba732241
Albert2019y <104762210+Albert2019y@users.noreply.github.com>
2022-05-11 08:32:55 +0200

    Update zoom.js

--- zoom.js
+++ zoom.js
@@ -33,7 +33,7 @@ function draw() {
   	let c = img.get(mouseX-0.5*s+i, mouseY-0.5*s+j);
   	fill(c);
     //Vi tegner de forstørrede pixels.
-	if(dist(mouseX,mouseY,mouseX-i,mouseY-j)<=s) {
+	if(dist(mouseX,mouseY,mouseX-i,mouseY-j)<=0.5*z*s) {
 		rect(mouseX - 0.5*z*s + z*i,mouseY - 0.5*z*s + z*j, z, z);
 
 	}
